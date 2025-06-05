0e8f13efb9a46974c19773842f4eb54830f5218d
Ferrayd <kunitskiisv@gmail.com>
2025-06-05 23:11:41 +0000

    Render

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,5 @@
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
-  include AuthConcern
   include Pundit::Authorization
   allow_browser versions: :modern
   helper_method :current_user, :signed_in?
