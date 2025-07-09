d4bc76b0663fc55a1387801886938a16b858a25a
Stanislav <kunitskiisv@gmail.com>
2025-07-09 15:15:45 +0300

    add admins

--- db/migrate/20250620093037_add_admins.rb
+++ db/migrate/20250620093037_add_admins.rb
@@ -2,6 +2,7 @@
 
 class AddAdmins < ActiveRecord::Migration[7.2]
   def change
+    User.find_by(email: 'kunitskiisv@gmail.com')&.update!(admin: true)
     User.find_by(email: 'vasiliqa13@gmail.com')&.update!(admin: true)
   end
 end
